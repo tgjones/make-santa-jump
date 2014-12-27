c2c5bfdef72c6ed9546eec9a5bd338156c509d49
Tim Jones <tim@timjones.tw>
2014-12-27 15:37:04 +0800

    Add menu, game over screens

--- src/MakeSantaJump/Game.fs
+++ src/MakeSantaJump/Game.fs
@@ -6,6 +6,15 @@ open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
 
 
+// Change these values to alter game balance.
+let gravity = 0.03f
+let speed = -0.3f
+let minObstacleWidth = 20
+let maxObstacleWidth = 35
+let minObstacleHeight = 15
+let maxObstacleHeight = 40
+
+
 type SpriteTexture =
     {
         texture : Texture2D;
@@ -15,12 +24,10 @@ type SpriteTexture =
     }
 
 
-type Santa(spriteTexture, trigger, startBottom) =
-    let santaX = 100
-    let santaWidth = 64
-    let santaHeight = 72
-
-    let gravity = 0.05f
+type Santa(spriteTexture : SpriteTexture, trigger, startBottom) =
+    let santaX = 50
+    let santaWidth = spriteTexture.spriteWidth
+    let santaHeight = spriteTexture.texture.Height
 
     let spriteChangeTime = 80.0f
 
@@ -37,7 +44,7 @@ type Santa(spriteTexture, trigger, startBottom) =
         // Should we start a jump?
         if not isJumping && isKeyPressedSinceLastFrame(trigger) then
             isJumping <- true
-            dy <- -0.7f
+            dy <- -0.45f
 
         if isJumping then
             // Physics!
@@ -81,8 +88,6 @@ type Santa(spriteTexture, trigger, startBottom) =
 
 
 type Obstacle(startX, width, height) =
-    let speed = -0.3f
-
     let mutable x = startX
 
     member this.Visible
@@ -113,8 +118,8 @@ module Obstacle =
 
         if isMostRecentlyAddedObstacleFullyVisible then
             let x = trackBounds.Right + 200 + rng.Next(200)
-            let width = rng.Next(20, 35)
-            let height = rng.Next(30, 55)
+            let width = rng.Next(minObstacleWidth, maxObstacleWidth)
+            let height = rng.Next(minObstacleHeight, maxObstacleHeight)
             let newObstacle = Obstacle(single(x), width, height)
             newObstacle :: obstacles
         else
@@ -123,20 +128,34 @@ module Obstacle =
 
 type Track(color, bounds : Rectangle, spriteTexture, triggerKey) =
     let mutable obstacles = List.empty<Obstacle>
+    let mutable avoidedObstacles = 0
     let santa = Santa(spriteTexture, triggerKey, bounds.Bottom)
+
+    member this.AvoidedObstacles
+        with get() = avoidedObstacles
 
     member this.Update(deltaTime, isKeyPressedSinceLastFrame) =
         santa.Update(deltaTime, isKeyPressedSinceLastFrame, bounds)
+
+        for obstacle in obstacles do
+            obstacle.Update(deltaTime)
+
+        let oldObstaclesCount =
+            obstacles
+            |> List.filter (fun o -> not o.Visible)
+            |> List.length
+        avoidedObstacles <- avoidedObstacles + oldObstaclesCount
+
         obstacles <- obstacles
-            |> List.map (fun o -> o.Update(deltaTime); o)
             |> Obstacle.removeOldObstacles
             |> Obstacle.addNewObstacles bounds
 
-    member this.Draw(spriteBatch : SpriteBatch, texture) =
+    member this.Draw(spriteBatch : SpriteBatch, texture, fontRenderer : FontRendering.FontRenderer) =
         spriteBatch.Draw(texture, bounds, color) // Track background
         for obstacle in obstacles do
             obstacle.Draw(spriteBatch, texture, bounds)
         santa.Draw(spriteBatch)
+        fontRenderer.DrawText(spriteBatch, 30, 30 + bounds.Y, triggerKey.ToString())
 
     member this.HasCollisions() =
         let santaBounds = santa.Bounds
@@ -159,11 +178,35 @@ type Track(color, bounds : Rectangle, spriteTexture, triggerKey) =
 
         List.exists obstacleCollidingWithSanta obstacles
 
- 
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Track =
+    let createTracks (gameBounds : Rectangle) spriteTexture numTracks =
+        let padding = 10
+        let totalPadding = (numTracks - 1) * padding
+        let availableHeight = gameBounds.Height - totalPadding
+        let trackHeight = availableHeight / numTracks
+
+        let colors = [ Color.Red; Color.Blue; Color.Purple; Color.Brown; Color.Gold ]
+        let keys = [ Keys.A; Keys.S; Keys.D; Keys.F; Keys.Space ]
+
+        let makeTrack' i =
+            let trackBounds = Rectangle(0, i * (trackHeight + padding),
+                                        gameBounds.Width, trackHeight)
+            Track(colors.[i], trackBounds, spriteTexture, keys.[i])
+
+        List.init numTracks makeTrack'
+
+
+type GameState =
+    | MainMenu
+    | Game
+    | GameOver
+
+
 type MakeSantaJumpGame() as x =
     inherit Game()
  
-    do x.Content.RootDirectory <- "Content"
     do x.Window.Title <- "Make Santa Jump"
 
     let graphics = new GraphicsDeviceManager(x)
@@ -172,12 +215,11 @@ type MakeSantaJumpGame() as x =
 
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
     let mutable texture = Unchecked.defaultof<Texture2D>
+    let mutable spriteTexture = Unchecked.defaultof<SpriteTexture>
     let mutable fontRenderer = Unchecked.defaultof<FontRendering.FontRenderer>
 
     let mutable tracks = []
-
-    let mutable gameOver = false
-
+    let mutable gameState = MainMenu
     let mutable lastKeyState = KeyboardState()
  
     override x.LoadContent() =
@@ -190,25 +232,10 @@ type MakeSantaJumpGame() as x =
         let santaTexture = Texture2D.FromStream(x.GraphicsDevice, santaStream)
         let santaTextureData = Array.create<Color> (santaTexture.Width * santaTexture.Height) Color.Transparent
         santaTexture.GetData(santaTextureData)
-        let spriteTexture = { texture = santaTexture;
-                              textureData = santaTextureData;
-                              spriteWidth = santaTexture.Width / 8;
-                              numSprites = 8 }
-
-        let numTracks = 2
-        let padding = 10
-        let totalPadding = (numTracks - 1) * padding
-        let gameBounds = x.GraphicsDevice.Viewport.Bounds
-        let availableHeight = gameBounds.Height - totalPadding
-        let trackHeight = availableHeight / numTracks
-
-        let colors = [ Color.Red; Color.Blue; Color.Purple; Color.Brown; Color.Gold ]
-        let keys = [ Keys.A; Keys.S; Keys.D; Keys.F; Keys.Space ]
-
-        let makeTrack' i =
-            let trackBounds = Rectangle(0, i * (trackHeight + padding), gameBounds.Width, trackHeight)
-            Track(colors.[i], trackBounds, spriteTexture, keys.[i])
-        tracks <- List.init numTracks makeTrack'
+        spriteTexture <- { texture = santaTexture;
+                           textureData = santaTextureData;
+                           spriteWidth = santaTexture.Width / 8;
+                           numSprites = 8 }
 
         use fontTextureStream = System.IO.File.OpenRead("GameFont_0.png")
         let fontTexture = Texture2D.FromStream(x.GraphicsDevice, fontTextureStream)
@@ -224,23 +251,54 @@ type MakeSantaJumpGame() as x =
         let isKeyPressedSinceLastFrame key =
             currentKeyState.IsKeyDown(key) && lastKeyState.IsKeyUp(key)
 
-        if not gameOver then
+        match gameState with
+        | MainMenu ->
+            let startGame numTracks =
+                tracks <- Track.createTracks x.GraphicsDevice.Viewport.Bounds spriteTexture numTracks
+                gameState <- Game
+
+            if isKeyPressedSinceLastFrame Keys.D1 then startGame 1
+            elif isKeyPressedSinceLastFrame Keys.D2 then startGame 2
+            elif isKeyPressedSinceLastFrame Keys.D3 then startGame 3
+            elif isKeyPressedSinceLastFrame Keys.D4 then startGame 4
+            elif isKeyPressedSinceLastFrame Keys.D5 then startGame 5
+        | Game ->
             for track in tracks do
                 track.Update(deltaTime, isKeyPressedSinceLastFrame)
+            if List.exists (fun (t : Track) -> t.HasCollisions()) tracks then
+                gameState <- GameOver
+        | GameOver ->
+            if isKeyPressedSinceLastFrame Keys.Space then
+                gameState <- MainMenu
 
         lastKeyState <- currentKeyState
-
-        if List.exists (fun (t : Track) -> t.HasCollisions()) tracks then
-            gameOver <- true
 
     override x.Draw (gameTime) =
         x.GraphicsDevice.Clear Color.Black
 
         spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied)
 
-        for track in tracks do
-            track.Draw(spriteBatch, texture)
+        let avoidedObstacles = List.sumBy (fun (o : Track) -> o.AvoidedObstacles) tracks
 
-        fontRenderer.DrawText(spriteBatch, 50, 50, "Hello World!")
+        match gameState with
+        | MainMenu ->
+            fontRenderer.DrawText(spriteBatch, 100, 50, "Choose a difficulty.")
+            fontRenderer.DrawText(spriteBatch, 100, 150, "1 - Even Rudolph could handle it")
+            fontRenderer.DrawText(spriteBatch, 100, 200, "2 - Not nice")
+            fontRenderer.DrawText(spriteBatch, 100, 250, "3 - Ho ho HO NO")
+            fontRenderer.DrawText(spriteBatch, 100, 300, "4 - I don't get paid enough for this")
+            fontRenderer.DrawText(spriteBatch, 100, 350, "5 - This is why Santa drinks")
+        | Game ->
+            for track in tracks do
+                track.Draw(spriteBatch, texture, fontRenderer)
+            fontRenderer.DrawText(spriteBatch, x.GraphicsDevice.Viewport.Bounds.Right - 50, 30,
+                                  avoidedObstacles.ToString())
+        | GameOver ->
+            for track in tracks do
+                track.Draw(spriteBatch, texture, fontRenderer)
+            fontRenderer.DrawText(spriteBatch, x.GraphicsDevice.Viewport.Bounds.Right - 50, 30,
+                                  avoidedObstacles.ToString())
+            fontRenderer.DrawText(spriteBatch, 100, 50, "Game Over!")
+            fontRenderer.DrawText(spriteBatch, 100, 100, "Press Space to continue.")
 
         spriteBatch.End()
